3b784e444f9a9337f2add4d932fea1f077e57442
akil udayakumar <akil@everycrave.me>
2017-07-21 18:10:46 +0530

    added directions to destination

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -5,6 +5,7 @@ import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng, CameraPosition } from '
 
 declare var google: any;
 
+
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html'
@@ -14,34 +15,86 @@ export class MapPage {
   // @ViewChild('map') map;
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  
+
+
+  
 
   constructor(private googleMaps: GoogleMaps, public navCtrl: NavController, public platform: Platform) { }
 
+  
 
   ngAfterViewInit() {
       this.updateLocation();
-      this.loadMap();
+      
   }
 
 
   updateLocation(){
-      navigator.geolocation.getCurrentPosition(function(success) {
-          console.log(success)
-      },function(err) {console.log(err)});
+      navigator.geolocation.getCurrentPosition((success)=> {
+          this.loadMap(success.coords)
+      },(err)=> {console.warn(err)});
   }
   
 
-  loadMap() {
+  loadMap(location) {
 
-      let latLng = new google.maps.LatLng(12.9290, 15);
+    let lat = location.latitude;
+    let long = location.longitude;
 
-      let mapOptions = {
-          center: latLng,
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
+    let latLng = new google.maps.LatLng(lat, long);
 
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
 
+    let directionsService = new google.maps.DirectionsService;
+    let directionsDisplay = new google.maps.DirectionsRenderer({draggable: true});
+    let destination = new google.maps.LatLng(12.97963047027588, 77.59068298339844); //Vidhana Soudha destination
+
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    //this.createMarker(latLng, this.map, "source");
+    //this.createMarker(destination, this.map, "destination");
+
+
+    this.getDirections(directionsService, directionsDisplay, latLng, destination)
   }
+
+
+
+
+  createMarker(pos, map, title){
+    var marker = new google.maps.Marker({
+      position: pos,
+      map: map,
+      title: title
+    });
+  }
+
+
+
+
+  getDirections(directionsService, directionsDisplay, source, destination) {
+
+    
+    directionsService.route({
+      origin:source,
+      destination: destination,
+      travelMode: 'DRIVING'
+    },(response, status)=> {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+        console.log(this.map)
+        directionsDisplay.setMap(this.map);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
+  
 }
